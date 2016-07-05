e514b1acdf9ff6dc185fd8c8afc38a7dbda1f9ac
Joe Bowbeer <joe.bowbeer@gmail.com>
2016-07-04 18:18:33 -0700

    Customizations

--- build.bat
+++ build.bat
@@ -1,26 +1,23 @@
 @echo off
 :: Renaming arguments
-set file_name=%1%
-set file_path=%2%
+set file_name=%~1%
+set file_path=%~2%
 
 :: Change this accordingly to your After Effects version
-set version=CC 2015
+set version=CC 2015.3
 
 :: Adobe After Effects folder location
-set base_path=C:\Program Files\Adobe
-set ae_folder_path=%base_path%\Adobe After Effects %version%
-set ae_scripts_folder_path=%ae_folder_path%\Support Files\Scripts
-
-
-cd "%file_path%"
+set base_path=D:\joebo\Creative Cloud Apps
+set ae_folder_path=%base_path%\Adobe After Effects %version%\Support Files
+set ae_scripts_folder_path=%ae_folder_path%\Scripts
 
 :: Copying script to Scripts folder
-copy "%file_name%" "%ae_scripts_folder_path%\%file_name%"
+REM copy "%file_path%\%file_name%" "%ae_scripts_folder_path%"
 
-cd "%ae_folder_path%\Support Files"
+cd "%ae_folder_path%"
 
-:: Running script in After Effect
-AfterFX -r "%ae_scripts_folder_path%\%file_name%"
+:: Running script in After Effects
+AfterFX -r "%file_path%\%file_name%"
 
 :: Printing happy feedback in the console
-echo "Successfully compiled %file_name% to %ae_scripts_folder_path%\%file_name%";
+echo AfterFX ran script "%file_path%\%file_name%"
